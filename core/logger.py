8029a9bde3f68e7c74baba0224fa5ec6e33404f2
miniUb <alexandrelim313@gmail.com>
2019-02-10 19:28:40 +0100

    New generic logger

--- core/logger.py
+++ core/logger.py
@@ -4,17 +4,26 @@ from logging import FileHandler
 from logging import Formatter
 from core.config.config import yeti_config
 
-LOG_FORMAT = (
-    "%(asctime)s [%(levelname)s]: %(message)s")
-LOG_LEVEL = logging.INFO
+init = False 
 
-# user logger
-USER_LOG_FILE = yeti_config.get('logging', 'filename')
-
-
+USER_LOG_FILE = yeti_config.get('logging', 'filename', None)
 userLogger = logging.getLogger("userLogger.messaging")
-userLogger.setLevel(LOG_LEVEL)
-userLogger_file_handler = FileHandler(USER_LOG_FILE)
-userLogger_file_handler.setLevel(LOG_LEVEL)
-userLogger_file_handler.setFormatter(Formatter(LOG_FORMAT))
-userLogger.addHandler(userLogger_file_handler)
+
+if USER_LOG_FILE != None :
+	init = True 
+	LOG_FORMAT = ("%(asctime)s [%(levelname)s]: %(message)s")
+	userLogger_file_handler = FileHandler(USER_LOG_FILE)
+	userLogger_file_handler.setFormatter(Formatter(LOG_FORMAT))
+	userLogger.setLevel(logging.INFO)
+	userLogger_file_handler.setLevel(logging.INFO)
+	userLogger.addHandler(userLogger_file_handler)
+
+def log(level, message):
+	if init == True:
+		logFunctions = {10:userLogger.debug,20:userLogger.info,30:userLogger.warning,40:userLogger.debug,50:userLogger.critical}
+		logFunction = logFunctions.get(level, None)
+		if logFunction != None: 
+			logFunction(message)
+
+	
+
